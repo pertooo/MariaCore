992e970d3352461c53e5b7199f22009346c66e4e
pertooo <pertiapertia@gmail.com>
2020-02-29 15:43:19 +0400

    First Push

---
+++ php/c_config.php
@@ -0,0 +1,7 @@
+<?php
+
+	define('BEAUTYPRESS_EMAIL', 'xpeedstudioteam@gmail.com');
+	define('BEAUTYPRESS_SUBJECT', 'New Contact Email');
+	define('BEAUTYPRESS_SUCCESS_MASSAGE', 'Successfully Sent Email');
+
+?>
